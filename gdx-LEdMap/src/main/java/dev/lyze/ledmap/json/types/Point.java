be65a25ca2556102140ff154ce8ddd05d4607141
Lyze <11274700+lyze237@users.noreply.github.com>
2020-10-01 20:44:33 +0200

    Added tostring to point

--- gdx-LEdMap/src/main/java/dev/lyze/ledmap/json/types/Point.java
+++ gdx-LEdMap/src/main/java/dev/lyze/ledmap/json/types/Point.java
@@ -8,4 +8,46 @@ public class Point {
         this.cx = cx;
         this.cy = cy;
     }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "cx=" + cx +
+                ", cy=" + cy +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point point = (Point) o;
+
+        if (getCx() != point.getCx()) return false;
+        return getCy() == point.getCy();
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getCx();
+        result = 31 * result + getCy();
+        return result;
+    }
+
+    public int getCx() {
+        return cx;
+    }
+
+    public void setCx(int cx) {
+        this.cx = cx;
+    }
+
+    public int getCy() {
+        return cy;
+    }
+
+    public void setCy(int cy) {
+        this.cy = cy;
+    }
 }

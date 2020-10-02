3579cbb026ac7e45b4fedf7e8f3212704c744198
Lyze <11274700+lyze237@users.noreply.github.com>
2020-10-02 19:38:57 +0200

    Fix jitpack circular dependency

--- example/core/src/main/java/dev/lyze/ledmap/example/GdxLEdMapTests.java
+++ example/core/src/main/java/dev/lyze/ledmap/example/GdxLEdMapTests.java
@@ -2,23 +2,12 @@ package dev.lyze.ledmap.example;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import dev.lyze.ledmap.LEdMap;
 
-import java.util.Arrays;
-
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class GdxLEdMapTests extends ApplicationAdapter {
-    private static final String[] files = new String[] {
-            "Entities.json",
-            "AutoLayers_1_basic.json",
-            "AutoLayers_2_stamps.json",
-            "AutoLayers_3_advanced.json",
-            "",
-    };
-
     @Override
     public void create() {
         LEdMap lEdMap = new LEdMap(Gdx.files.internal("Typical_2D_platformer_example.json"));
+        System.out.println("HI");
     }
 }
